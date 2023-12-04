e14b7720b6ba7310ae3c618b30cdbe198313cd61
= <=>
2023-12-04 17:29:48 -0300

    Finalizado os EndPoints da Classe Categoria

--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -1,4 +1,5 @@
 using ApiCatalogo.Context;
+using ApiCatalogo.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -23,11 +24,36 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
+app.MapGet("/", () => "Catalogo de Produtos 2023");
 
+app.MapGet("/categorias", async (AppDbContext db) => await db.Categorias.ToListAsync());
+
+
+app.MapGet("/categorias/{id:int}", async (int id, AppDbContext db) =>
+    await db.Categorias.FindAsync(id) is Categoria categoria ? Results.Ok(categoria) : Results.NotFound("Categoria não encontrada."));
+
+
+app.MapPost("/categorias", async (Categoria categoria, AppDbContext db) =>
+{
+    db.Categorias.Add(categoria);
+    await db.SaveChangesAsync();
+    return Results.Created($"/categorias/{categoria.CategoriaId}", categoria);
+});
+
+app.MapPut("/categorias/{id:int}", async (int id, Categoria categoria, AppDbContext db) =>
+{
+    var _categoria = await db.Categorias.FindAsync(id);
+
+    if (_categoria is null) return Results.NotFound("Categoria não encontrda");
+
+    _categoria.Nome = categoria.Nome;
+    _categoria.Descricao = categoria.Descricao;
+
+    await db.SaveChangesAsync();
+
+    return Results.Ok("Categoria alterada com sucesso.");
+
+});
 
 app.Run();
 
